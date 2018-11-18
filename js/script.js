54bbce53228da81516b9348d73244978310557ab
ArtemBreat <artoym.zan@gmail.com>
2018-11-18 13:04:21 +0300

    создание аккардиона

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
-window.onload = function () {
-    send.onclick = function () {
-   alert("Спасибо за доверие.Мы свяжемся с Вами максимально оперативнно")
-    }
-}
+$(document).ready(function () {
+    $(".card").mouseover(function () {
+        $(this).find(".thumbnail").stop().fadeIn()
+    });
+    $(".card").mouseout(function () {
+        $(this).find(".thumbnail").stop().fadeOut()
+    });
+
+    var accord = $(".accordion");
+    accord.find(".title:not(.active)").siblings("div").slideUp();
+    accord.find(".title").on("click", function () {
+        $(this).siblings("div").stop().slideToggle(500);
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+        }
+        else {
+            $(this).addClass("active");
+        }
+    });
+
+    var btnUp =$(".up");
+    btnUp.hide();
+    $(window).scroll(function () {
+        if($(this).scrollTop() > 50){
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+
+        }
+
+    })
+    btnUp.click(function () {
+        $("body,html").animate({
+            scrollTop:0
+        }, 500);
+        return false;
+    })
+
+
+})
+
+
+
+
